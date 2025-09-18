f156682753e77c099f8b66d675abc8b2dda3bef2
huyctran <58238572+huyctran@users.noreply.github.com>
2025-09-18 13:30:55 -0700

    Git Hub Actions

--- .github/actions/shopify-push/push_themekit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-theme deploy \
-  --password="$SHOPIFY_CLI_THEME_TOKEN" \
-  --store="$SHOPIFY_FLAG_STORE" \
-  --themeid="$SHOPIFY_FLAG_THEME_ID" \
-  --dir="$SHOPIFY_FLAG_PATH" \
-  ${SHOPIFY_FLAG_IGNORE:+--ignored-file="$SHOPIFY_FLAG_IGNORE"}
